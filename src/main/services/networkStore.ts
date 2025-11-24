8e8b995c00a3890dd7770a29a71287bcdbe60ba8
kapil rajput <gemlay.dev4@gemlay.com>
2025-11-24 11:28:27 +0530

    r

--- src/main/services/networkStore.ts
+++ src/main/services/networkStore.ts
@@ -4,17 +4,17 @@ import { broadcast } from '../ipc/broadcast'
 
 export const networkStore = {
   logs: [
-    {
-      data: { method: 'Get', url: 'https://api.testgemlay.com/customer_review' },
-      type: 'networkRequest'
-    },
-    {
-      data: {
-        method: 'Post',
-        url: 'https://stats2.mytuner.mobi/api/v2/web-api/play',
-        body: { connect_uuid: 'a4f81ca2-b4e2-48cc-bb53-ae4386ed7dbf' }
-      }
-    }
+    // {
+    //   data: { method: 'Get', url: 'https://api.testgemlay.com/customer_review' },
+    //   type: 'networkRequest'
+    // },
+    // {
+    //   data: {
+    //     method: 'Post',
+    //     url: 'https://stats2.mytuner.mobi/api/v2/web-api/play',
+    //     body: { connect_uuid: 'a4f81ca2-b4e2-48cc-bb53-ae4386ed7dbf' }
+    //   }
+    // }
   ] as any,
   autoClearLength: 301,
   pauseNetwork: false,
